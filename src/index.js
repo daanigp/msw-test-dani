88b116002280d80346ee6532f80ffd8bc30a44fc
Carlos Agullo <carlos.agullo@gmail.com>
2024-04-16 00:13:03 +0200

    getPMA1TestCaseExecution devuelve JSON con datos falsos

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ if(process.env.NODE_ENV === 'development') {
 
 const app = express()
 
-app.get('/todos', mockServer.getTodos) // Respuesta mockeada a la ruta
+app.get('/todos', mockServer.getTodos) // Respuesta mockeada a la ruta -> curl --get http://localhost:8080/todos | jq
 
-app.get('/zephyr/PMA1-:nombre', mockServer.getPMA1TestCaseInfo)
-app.get('/zephyr/testCaseExecutions/PMA1-*', mockServer.getPMA1TestCaseExecution)
+app.get('/zephyr/PMA1-:nombre', mockServer.getPMA1TestCaseInfo) // curl --get http://localhost:8080/zephyr/PMA1-T33 | jq
+app.get('/zephyr/testCaseExecutions/PMA1-:nombre', mockServer.getPMA1TestCaseExecution) // curl --get http://localhost:8080/zephyr/testCaseExecutions/PMA1-T33 | jq
 
 app.listen(8080, () => {
     console.log(process.env.NODE_ENV)
--- src/mockServer.js
+++ src/mockServer.js
@@ -21,11 +21,11 @@ module.exports = {
             const nombreFalso = faker.name.findName();
 
             const PMA1 = {
-                "id": generarNumeroAleatorio(),
+                "id": generarNumeroAleatorio(100000000),
                 "key": "PMA1-"+url,
                 "name": nombreFalso,
                 "project": {
-                    "id": generarNumeroAleatorio(),
+                    "id": generarNumeroAleatorio(100000000),
                     "self": "https://api.zephyrscale.smartbear.com/v2/projects/129674"
                 },
                 "createdOn": generarFechaAleatoria(),
@@ -34,11 +34,11 @@ module.exports = {
                     "e2e_ui",
                 ],
                 "status": {
-                    "id": generarNumeroAleatorio(),
+                    "id": generarNumeroAleatorio(100000000),
                     "self": "https://api.zephyrscale.smartbear.com/v2/statuses/2381907"
                 },
                 "folder": {
-                    "id": generarNumeroAleatorio(),
+                    "id": generarNumeroAleatorio(100000000),
                     "self": "https://api.zephyrscale.smartbear.com/v2/folders/3251612"
                 },
                 "owner": null,
@@ -52,8 +52,22 @@ module.exports = {
     },
 
     getPMA1TestCaseExecution: async (req, res) => {
+        //Crear un json con fker, y devolverlo.
         try {
-            return res.send("calling getPMA1TestCaseExecution") 
+            const url = req.params.nombre;
+            const resultadosMax = generarNumeroAleatorio(100);
+            const resultadosTotal = generarNumeroAleatorioMenorQueOtro(20, resultadosMax);
+
+            const PMA1 = {
+                "next": null,
+                "startAt": 0,
+                "maxResults": resultadosMax,
+                "total": resultadosTotal,
+                "isLast": generarBooleanoAleatorio(),
+                "values": generarPMA1(url, resultadosTotal)
+            }
+
+            return res.json(PMA1)
         } catch (err) {
             console.error(err);
             return res.status(400).send("Ha ocurrido un error al generar el nombre.")
@@ -61,10 +75,30 @@ module.exports = {
     }
 }
 
-function generarNumeroAleatorio() {
-    const numero = Math.floor(Math.random() * (100000000 - 1) + 1);
+function generarNumeroAleatorio(num) {
+    const numero = Math.floor(Math.random() * (num - 1) + 1);
 
     return numero;    
+}
+
+function generarNumeroAleatorioMenorQueOtro(num, numeroMax) {
+    let numero;
+
+    do {
+        numero = Math.floor(Math.random() * (num - 1) + 1);
+    } while (numero > numeroMax)
+    
+    return numero;    
+}
+
+function generarBooleanoAleatorio() {
+    const numeroAleatorio = Math.round(Math.random());
+
+    if (numeroAleatorio === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function generarFechaAleatoria() {
@@ -83,4 +117,52 @@ function generarFechaAleatoria() {
     fechaAleatoria.setHours(horas, minutos, segundos, milisegundos);
     
     return fechaAleatoria;
+}
+
+function generarPMA1(url, numVeces) {
+    const objetosPMA1 = [];
+
+    for (let i = 1; i <= numVeces; i++) {
+        const pma = {
+            "id": i,
+            "key":"PMA1-" + url + "-E" + generarNumeroAleatorio(10000),
+            "project": {
+                "id":generarNumeroAleatorio(100000000),
+                "self":"https://api.zephyrscale.smartbear.com/v2/projects/129674"
+            },
+            "testCase": {
+                "self":"https://api.zephyrscale.smartbear.com/v2/testcases/PMA1-T25/versions/1",
+                "id":generarNumeroAleatorio(100000000)
+            },
+            "environment":null,
+            "jiraProjectVersion":null,
+            "testExecutionStatus": {
+                "id":generarNumeroAleatorio(100000000),
+                "self":"https://api.zephyrscale.smartbear.com/v2/statuses/2381896"
+            },
+            "actualEndDate":generarFechaAleatoria(),
+            "estimatedTime":null,
+            "executionTime":33582,
+            "executedById":"5dee5bf9dfde6b0e555b5be0",
+            "assignedToId":"5dee5bf9dfde6b0e555b5be0",
+            "comment":null,
+            "automated":true,
+            "testCycle": {
+                "self":"https://api.zephyrscale.smartbear.com/v2/testcycles/17179767",
+                "id":generarNumeroAleatorio(100000000)
+            },
+            "customFields": {
+                "Source of the problem":null,
+                "Action item":null
+            },
+            "links": {
+                "self":"https://api.zephyrscale.smartbear.com/v2/testexecutions/698777302/links",
+                "issues":[]
+            }
+        }
+
+        objetosPMA1.push(pma)
+    }
+
+    return objetosPMA1;
 }
